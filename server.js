a6d68acdb58b1f1e7531f71274b72f30cb6ff042
Max Fierke <max@maxfierke.com>
2014-12-12 17:42:47 -0600

    Replace gzippo with compression and serve-static
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-var gzippo = require('gzippo');
+var serveStatic = require('serve-static');
+var compression = require('compression');
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
 
 app.use(morgan('dev'));
-app.use(gzippo.staticGzip(__dirname + "/dist"));
-
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + "/dist/index.html");
-});
-
+app.use(compression());
+app.use(serveStatic('dist/', {'index': ['index.html']}));
 app.listen(process.env.PORT || 5000);
 
 console.log("LibedMe listening on port %d", process.env.PORT || 5000);
